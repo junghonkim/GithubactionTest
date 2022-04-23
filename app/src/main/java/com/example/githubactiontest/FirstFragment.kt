cf476c734d3c36198fa6b9a125a26415320c1f8a
junghoon.kim <luckyhuni@gmail.com>
2022-04-23 22:35:43 +0900

    master commit2

--- app/src/main/java/com/example/githubactiontest/FirstFragment.kt
+++ app/src/main/java/com/example/githubactiontest/FirstFragment.kt
@@ -1,6 +1,7 @@
 package com.example.githubactiontest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -35,6 +36,8 @@ class FirstFragment : Fragment() {
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        Log.e(">>>>","master log cmmit")
     }
 
     override fun onDestroyView() {
